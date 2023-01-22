9cfe4b26d6459f4ab344b0e551535bcfe700d7ba
Cedric Pintado <pintadocedric@gmail.com>
2023-01-22 10:11:15 +0100

    base de donnees

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,38 @@
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+const cors = require('cors');
 
-app.get("/", (req,res) => {
-    res.json({message : "yo"
-    })
-} )
+mongoose.connect ('mongodb+srv://piiquante:piiquante@cluster0.aw1rvuo.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log('MongoDB connecté');
+})
+.catch(() => {
+    console.log('MongoDB erreur');
+});
+
+const userSchema = new Schema({
+    email: String,
+    password: String,
+});
+
+const User = mongoose.model('User', userSchema);  
+
+app.use(cors());
+
+app.use(express.json());
+
+app.post('/api/auth/singup', (req,res) =>{
+    console.log(req.body);
+    res.send('ok');
+});
+
+app.get("/", async (req,res) => {
+    await User.create({ email: 'example@test.test', password: 'test'});
+    res.send('ok');
+});
 
 app.listen("3000", () => {
-    console.log("serveur ok!")
-} )
+    console.log("serveur ok")
+});
