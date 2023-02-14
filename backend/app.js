fb2a4fbd206bc809d4deb24cfa018c07bebb8514
Cedric Pintado <pintadocedric@gmail.com>
2023-02-14 17:17:30 +0100

    token

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
-mongoose.connect ('mongodb+srv://piiquante:piiquante@cluster0.aw1rvuo.mongodb.net/?retryWrites=true&w=majority')
+mongoose
+.connect ("mongodb+srv://piiquante:piiquante@cluster0.aw1rvuo.mongodb.net/?retryWrites=true&w=majority")                 
 .then(() => {
     console.log('MongoDB connecté');
 })
@@ -26,29 +28,75 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);  
 
-//middleware permettant au frontend a se connecter a l'API//
+//middleware permettant au frontend de se connecter a l'API//
 app.use(cors());
 
 app.use(express.json());
 
 //creation d'un nouvel utilisateur dans la base de données//
-app.get("/", async (req,res) => {
-    await User.create({ email: 'example@test.test', password: 'test'});
+app.get('/', async (req, res) => {
+    await User.create({ email: "example@test.tes", password: "test"});
     res.send('ok');
 });
 
 
 //Route signup recuperation de la creation du login utilisateur + hashage password//
-app.post('/api/auth/signup', async (req,res) => {
-    console.log(req.body);
-    bcrypt.hash(req.body.password, 10)
-     .then (async (hash) => {
-        await User.create({ email: req.body.email, password: hash });
-        res.status(201).json({ message: "Uilisateur créé" })
+app.post('/api/auth/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({error: "Veuillez remplir tous les champs"});
+    }
+
+ bcrypt
+    .hash(req.body.password, 10)
+    .then (async (hash) => {
+        const user = await User.create({ email: req.body.email, password: hash });
+        if(!user) {
+            return res.status(400).json({ error: "Erreur lors de la création de l'utilisateur" });
+        }
+        return res.status(201).json({ message: "Uilisateur créé" });
      }) 
      .catch(() => {
         res.status(500).json({ error: "Erreur lors du hashage du mot de passe" });
-     }); 
+     });   
 });
 
+//Route login + token stockage de la session utilisateur 
+app.post('/api/auth/login', (req, res) => {
+    const { email, password } = req.body;
 
+    if(!email || !password) {
+        return res.status(400).json({error: "Veuillez remplir tous les champs"});
+    }
+
+    User.findOne({ email : email }).then((user) => {
+        if(!user) {
+            return res.status(404).json({error : "Utilisateur introuvable"});
+        }
+
+        bcrypt
+        .compare(password, user.password)
+        .then((valid) => {
+            if(!valid){
+               return res.status(400).json({error : "Mot de passe non valide"});
+            }
+
+            return res.status(200).json({
+                userId : user._id,
+                token: jwt.sign(
+                 {
+                userId: user.id,
+                 },     
+                "SECRET_TOKEN",
+                 {
+                expiresIn: "24h",
+                 }
+                ),
+            });
+        })  
+        .catch(() => {
+            return res.status(500).json({ error : "Erreur lors de la comparaison du mot de passe" });
+        });
+    });
+});
