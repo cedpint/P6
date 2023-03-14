1e3faaf135691b1c320b22d4a701e2086fc6982f
Cedric Pintado <pintadocedric@gmail.com>
2023-03-14 18:12:28 +0100

    auth

--- backend/routes/sauce.route.js
+++ backend/routes/sauce.route.js
@@ -3,7 +3,7 @@ const express = require("express");
 const Sauce = require("../models/sauce.model");
 const multer = require("../middleware/multer-config"); 
 const sauce = require("../models/sauce.model");
-const auth = require('auth');
+
 
 const router = express.Router();
 
@@ -12,21 +12,21 @@ const router = express.Router();
 const sauceCtrl = require('../controllers/sauce');
 
 //Route sauce qui affiche toutes les sauces
-router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/', sauceCtrl.getAllSauces);
 
 //Fonction permettant d'appeler une sauce
-router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
 
 //Fonction permettant de créer des sauces
-router.post('/', auth, multer, sauceCtrl.createSauce);
+router.post('/', multer, sauceCtrl.createSauce);
 
 //Fonction permettant de modifier une sauce
-router.put('/:id', auth, multer, sauceCtrl.modifySauce );
+router.put('/:id', multer, sauceCtrl.modifySauce );
 
 //Fonction permettant de supprimer une sauce
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
 
 //Fonction permettant de liker et disliker une sauce
-router.post('/:id/like', auth, sauceCtrl.likeDislike);
+router.post('/:id/like', sauceCtrl.likeDislike);
 
 module.exports = router ;
